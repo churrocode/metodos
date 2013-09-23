a144cbe5dca91c05361a50a482990bbf0895a1a7
ChurroCode <code@churro.com>
2013-09-22 23:41:28 -0300

    fix MatrizBanda.h

--- TP2/src/MatrizBanda.h
+++ TP2/src/MatrizBanda.h
@@ -2,8 +2,9 @@
 #define	MATRIZBANDA_H
 
 #include <list>
-#include <pair>
-typedef num double;
+#include <utility>
+#include <vector>
+typedef double num;
 #define eps 10e-12
 #define iguales(x, y) (- eps < x - y  && x -y < eps)
 
@@ -12,16 +13,17 @@ using namespace std;
 class MatrizBanda {
     class Fila{
     public:
-        list<pair<int, num>> noNulos;
+        list<pair<int, num> > noNulos;
         void sumarMultiploDeFila(const Fila& f1, num m);
         num get(const int j);
+        void set(const int j, const num a); //a != 0, por favor!
     };
 public:
     MatrizBanda(int n, int m, int p, int q);
     MatrizBanda(const MatrizBanda& orig);
     virtual ~MatrizBanda();
-    num get(i,j);
-    void set(i,j, num);
+    num get(int i, int j);
+    void set(int i, int j, num a);
     void sumarMultiploDeFila(int i1, int i2, num k);
     //operator[](int i, int j);
 private:
