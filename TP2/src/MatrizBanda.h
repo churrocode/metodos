a81901ef9a6bcc3d52a6a6c6b42a829982a5a3c6
ChurroCode <code@churro.com>
2013-09-22 23:10:10 -0300

    matriz con filas por listas

--- TP2/src/MatrizBanda.h
+++ TP2/src/MatrizBanda.h
@@ -1,20 +1,35 @@
 #ifndef MATRIZBANDA_H
 #define	MATRIZBANDA_H
 
+#include <list>
+#include <pair>
+typedef num double;
+#define eps 10e-12
+#define iguales(x, y) (- eps < x - y  && x -y < eps)
+
+using namespace std;
+
 class MatrizBanda {
+    class Fila{
+    public:
+        list<pair<int, num>> noNulos;
+        void sumarMultiploDeFila(const Fila& f1, num m);
+        num get(const int j);
+    };
 public:
     MatrizBanda(int n, int m, int p, int q);
     MatrizBanda(const MatrizBanda& orig);
     virtual ~MatrizBanda();
-    float* getValues();
+    num get(i,j);
+    void set(i,j, num);
+    void sumarMultiploDeFila(int i1, int i2, num k);
     //operator[](int i, int j);
 private:
     int n;
     int m;
     int p;
     int q;
-    float values[];
+    vector<Fila> filas;
 };
 
 #endif	/* MATRIZBANDA_H */
-
