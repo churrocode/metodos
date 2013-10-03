c18741d39e578f8a73dd6559fa459ad3bc575d45
ChurroCode <code@churro.com>
2013-10-03 02:16:38 -0300

    ajustar el valor de la banda

--- TP2/src/Puente.cpp
+++ TP2/src/Puente.cpp
@@ -120,7 +120,7 @@ vector<num>* Puente::resolverPuente() {
         cout << 2*(i-1)+1 << " -> " << ((i % 2 == 1) ? (-1*cargas[i/2 -1]) : 0) << endl;
     }
     matriz.printMatriz();
-    matriz.triangularConGauss(6, *b);
+    matriz.triangularConGauss(4, *b);
     matriz.printMatriz();
     vector<num>* sol = backwardSubstitution(matriz, *b);
     
