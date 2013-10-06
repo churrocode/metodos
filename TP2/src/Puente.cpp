96cb6ce76c8f0365abc25c81e8e23b4cbfab25d1
jgarrone <javier3653@gmail.com>
2013-10-06 17:30:26 -0300

    informe resultados

--- TP2/src/Puente.cpp
+++ TP2/src/Puente.cpp
@@ -135,7 +135,7 @@ bool Puente::esSeguro() {
 double Puente::costo() {
     double costo = 0;
     costo += this->n * this->hip; //los n links oblicuos;
-    costo += (this->n + (this->n > 2 ? this->n - 2 : 0) * (this->span / this->n); //los links horizontales de abajo (n) + los de arriba (n-2, si existen)
+    costo += (this->n + (this->n > 2 ? this->n - 2 : 0) * (this->span / this->n)); //los links horizontales de abajo (n) + los de arriba (n-2, si existen)
     costo += this->h * (n-1); 
     return costo;
 }
