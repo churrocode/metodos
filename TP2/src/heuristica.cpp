1ddd57f330b48979a6cdfe3c2c0f5001ed27a4e6
Ariel <darkf0e@darkf0e.(none)>
2013-10-06 23:46:12 -0300

    multiplicar subestructuras-1

--- TP2/src/heuristica.cpp
+++ TP2/src/heuristica.cpp
@@ -104,7 +104,8 @@ pair<double, pair< vector<double>, vector<double> > > costoTotal(Puente& puente)
 	pair<double,int> costo_y_pilares = calcularCostoEstructura(puente, posiciones_pilares, costo_subestructuras);
 	double costo_estructuras = costo_y_pilares.first;
 	int cant_pilares = costo_y_pilares.second;
-        double costo_total = costo_estructuras + (cant_pilares > 0) ? (cant_pilares-1) * costo_pilar : 0;
+    int cant_subestructuras = cant_pilares+1;
+        double costo_total = costo_estructuras + cant_subestructuras * costo_pilar;
         pair<vector<double>, vector<double> > pilares_y_sub;
         pilares_y_sub.first = posiciones_pilares;
         pilares_y_sub.second = costo_subestructuras;
