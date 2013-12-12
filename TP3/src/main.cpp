c4124778ed42f08f5fad34bf9099dd83670e8106
Ariel <darkf0e@DarkUbuntu.(none)>
2013-12-12 20:23:29 -0300

    acomode el main para que el c se pueda pasar por parametro al igual que si se quiere medir o no

--- TP3/src/main.cpp
+++ TP3/src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "MatrizEsparsa.h"
 #include "factorizacionQR.h"
 #include "quadraticExtrapolation.h"
@@ -9,14 +10,14 @@
 
 #define epsilonDeParada 1e-8
 #define cadaCuantoQE 7
-#define ponderadorC 0.2
+//#define ponderadorC 0.95
 
 using namespace std;
 
 int NODOS;
 int LINKS;
 
-void metodoDeLaPotencia(MatrizEsparsa& , num, bool, bool, vector<num>&);
+void metodoDeLaPotencia(MatrizEsparsa& , bool, bool, vector<num>&,num,int=cadaCuantoQE);
 bool corresponde_usar_extrapolacion(const int iters, const int n);
 void extrapolacion_cuadratica(
     vector<num>& autovector_nuevo,
@@ -26,18 +27,23 @@ void extrapolacion_cuadratica(
 ); //vector_nuevo:in = estimación de la iteración actual; vector_nuevo:out = extrapolación usando los otros tres.
 
 int main(int argc, char** argv) {
-    if(argc < 2) {
+    if(argc < 3) {
         cout << "Uso: " << "Pasar un archivo de entrada, leer README.txt para ver el formato" << endl;
         return 0;
     }
+
+
     ifstream archivo_entrada;
     cout << argv[1] << endl;
     archivo_entrada.open(argv[1]);
     
-    bool usarQE = false;
-    if (argc >= 3) {
-        usarQE = true;
-    }
+    float ponderadorC; // el tercer argumento de la consola es el c
+    sscanf(argv[2],"%f",&ponderadorC);
+    
+    int arg4, arg5;
+    sscanf(argv[3],"%d",&arg4); sscanf(argv[4],"%d",&arg5);
+    bool usarQE = arg4 == 1; // el cuarto argumento determina si se usa qe, igual a 1 <-> se usa, != 1 <-> no se usa. Tiene que ser un numero
+    bool medir = arg5 == 1;  // el quinto argumento determina si se miden resultados, idem que usarQE.
 
     int cantidad_paginas, cantidad_links;
     // primer int: cantidad de paginas
@@ -60,34 +66,24 @@ int main(int argc, char** argv) {
     }
     archivo_entrada.close();
 
-    //P.printMatriz();
-
-    // genero P estocastica
-    //HORRIBLE!!!!! HACERLO ADENTRO DE MATRIZ ESPARSA!!!!!!!! LPMQLRMP!!!!
-    /*for(int j = 0; j < P.getDimFilas(); j++){   
-        num col_sum = 0;
-        for(int i = 0; i < P.getDimColumnas(); i++){
-            col_sum += P.get(i, j); 
-        }
-        if(col_sum != 0){
-            for(int k = 0; k < P.getDimFilas(); k++){
-                P.set(k, j, P.get(k, j)/col_sum);
-                }
-        }
-    }*/
     P.estocastizar();
     vector<num> autovector;
-                //                      v BOOL MEDIR      
-    metodoDeLaPotencia(P, ponderadorC, usarQE, true, autovector);
-                //                ^ BOOL USAR EXTRAPOLACION
+                //                   v BOOL MEDIR      
+    metodoDeLaPotencia(P, usarQE, medir, autovector, ponderadorC);
+                //            ^ BOOL USAR EXTRAPOLACION
                                                                 
-    //imprimirVector(autovector);
-    
     ofstream archivo_resultados;
-    if (usarQE) archivo_resultados.open("../parser/resultadosConQE0.2.out");
-    else archivo_resultados.open("../parser/resultadosSinQE0.2.out");
+    string nombre_archivo_resultado_conQE = "../parser/resultadosConQE";
+    string nombre_archivo_resultado_sinQE = "../parser/resultadosSinQE";
+    string extension = ".out";
+    char ponderadorC_str[21];
+    sprintf(ponderadorC_str,"%g",ponderadorC);
+    nombre_archivo_resultado_conQE += ponderadorC_str + extension;
+    nombre_archivo_resultado_sinQE += ponderadorC_str + extension; 
+
+    if (usarQE) archivo_resultados.open(nombre_archivo_resultado_conQE.c_str());//archivo_resultados.open("../parser/resultadosConQE0.2.out");
+    else archivo_resultados.open(nombre_archivo_resultado_sinQE.c_str());//archivo_resultados.open("../parser/resultadosSinQE0.2.out");
     
-    //pair<num, int> p;
     vector<pair<num, int> > autovector_indexado(autovector.size());
     for (int i = 0; i < autovector.size(); ++i) {
         autovector_indexado[i] = pair<num, int>(autovector[i], i);
@@ -95,7 +91,6 @@ int main(int argc, char** argv) {
     sort(autovector_indexado.begin(), autovector_indexado.end());
 
     for(int i = 0; i < autovector_indexado.size(); i++) {
-        //p = sacarMaximo(autovector); //esto es selection sort!!!!! O(n^2) con n arriba de 100000!!!!!
         archivo_resultados << autovector_indexado[i].second << " "  << autovector_indexado[i].first  << endl;
     }
     
@@ -104,44 +99,32 @@ int main(int argc, char** argv) {
 }
 
 
-/*num normaUno(vector<num>& el_vector) {
-    num norma = 0;
-    for(int i = 0; i < el_vector.size(); ++i) {
-        norma += abs(el_vector[i]);
-    }
-    return norma;
-}
-
-*/void restaVectores(vector<num>& v1, vector<num>& v2) {
+void restaVectores(vector<num>& v1, vector<num>& v2) {
     for(int i = 0; i < v1.size(); ++i) {
         v1[i] -= v2[i];
     }
 }
 
-/*num diferencia_normaUno(const vector<num>& v1, const vector<num>& v2) {
-    num acum = 0;
-    for (int i = 0; i < v1.size(); ++i){
-        acum += abs(v1[i]-v2[i]);
-    }
-    return acum;
-}*/
-
-/*num productoInterno(vector<num>& v1, vector<num>& v2) ^{
- 
-}*/
-        
-void metodoDeLaPotencia(MatrizEsparsa& P, num c, bool usar_extrapolacion, bool medir, vector<num>& autovector) {
+// el c y la periodicida de la aplicacion de qe son parametros por defecto, ver el prototipo de la funcion mas arriba
+void metodoDeLaPotencia(MatrizEsparsa& P, bool usar_extrapolacion, bool medir, vector<num>& autovector, num c, int cada_cuanto_qe) {
     ofstream archivo_mediciones;
     if(medir) {    
-        if (usar_extrapolacion) archivo_mediciones.open("../parser/medicionesConQE0.2.out");
-        else archivo_mediciones.open("../parser/medicionesSinQE0.2.out");
+        string nombre_archivo_mediciones_conQE = "../parser/medicionesConQE";
+        string nombre_archivo_mediciones_sinQE = "../parser/medicionesSinQE";
+        string extension = ".out";
+        char ponderadorC_str[21];
+        sprintf(ponderadorC_str,"%g",c);
+        nombre_archivo_mediciones_conQE += ponderadorC_str + extension;
+        nombre_archivo_mediciones_sinQE += ponderadorC_str + extension; 
+        if (usar_extrapolacion) archivo_mediciones.open(nombre_archivo_mediciones_conQE.c_str());
+        else archivo_mediciones.open(nombre_archivo_mediciones_sinQE.c_str());
         archivo_mediciones << NODOS << endl;
         archivo_mediciones << LINKS << endl;
     }
+
     int cantidad_paginas = P.getDimFilas(); // la matriz es cuadrada(cant_paginas * cant_paginas)
     num proba = 1.0 / cantidad_paginas;
 
-    //vector<num> vector_proba_uniforme = vector<num>(cantidad_paginas,proba);
     autovector = vector<num>(cantidad_paginas,proba);
     vector<num> autovector_nuevo = vector<num>(cantidad_paginas);
     
@@ -150,51 +133,32 @@ void metodoDeLaPotencia(MatrizEsparsa& P, num c, bool usar_extrapolacion, bool m
 
     int cant_iters = 1;
     num error = 1/0.0;
-    //bool seguir_iterando = true;
     time_t inicio, fin;
-    num tiempo_ex_en_milisegundos;
+    num tiempo_ex_en_segundos;
     inicio = time(NULL);
     while(error >= epsilonDeParada) {
-        /*for(int j = 0; j < cantidad_paginas; j++){ // multiplicamos: P * autovector
-
-            num producto_interno = 0;
-            for(int i = 0; i < cantidad_paginas; i++){
-                producto_interno += P.get(j,i)*autovector[i];
-
-            }
-            //autovector_temp es la iteración que acabamos de calcular
-            autovector_nuevo[j] = producto_interno*c;
-        }*/
+        
         P.multiplicar_vector(autovector, autovector_nuevo);
         escalar_vector(autovector_nuevo, c);
-        //cout << "xk+1: "; imprimirVector(autovector_nuevo); cout << endl;
-        //cout << "xk: "; imprimirVector(autovector); cout << endl;
         num norma_autovector_nuevo = normaUno(autovector_nuevo); //cout << "norma xk+1: " << norma_autovector_nuevo << endl;
         num norma_autovector = normaUno(autovector); //cout << "norma xk: " << norma_autovector << endl;
         num w = norma_autovector - norma_autovector_nuevo;
         
-        //cout << "v: "; imprimirVector(vector_proba_uniforme); cout << endl;
-        //vector<num> w_por_v = multPorEscalar(vector_proba_uniforme,w);
-        //cout << "w_por_v: "; imprimirVector(w_por_v); cout << endl;
-
-        for(int j = 0; j < cantidad_paginas; j++) {   
-            // autovector_nuevo[j] += w_por_v[j];
+        for(int j = 0; j < cantidad_paginas; j++) {  
             autovector_nuevo[j] += w*proba;
         }
         
-        if (usar_extrapolacion && corresponde_usar_extrapolacion(cant_iters, cadaCuantoQE)) {
+        if (usar_extrapolacion && corresponde_usar_extrapolacion(cant_iters, cada_cuanto_qe)) {
             cout << "Entre a QE " << endl;
             archivo_mediciones << "QE ";
             quadraticExtrapolation(autovector_nuevo, autovector, autovector_anteultimo, autovector_antepenultimo);
         }
 
         error = diferencia_normaUno(autovector, autovector_nuevo);
-        //seguir_iterando = error >= epsilonDeParada;
         
         if(medir) {
             archivo_mediciones << cant_iters << " " << error << endl;
         }
-
 
         //reacomodamos los vectores:
         if (usar_extrapolacion) {
@@ -205,25 +169,22 @@ void metodoDeLaPotencia(MatrizEsparsa& P, num c, bool usar_extrapolacion, bool m
         //el autovector es la iteración que acabamos de calcular
         autovector = autovector_nuevo;
         ++cant_iters;
-//imprimirVector(autovector);
     }
 
 
     fin = time(NULL);
-    tiempo_ex_en_milisegundos = (fin - inicio) * 1000000.0;
+    tiempo_ex_en_segundos = (fin - inicio);
 
-    cout << "TIEMPO: " << tiempo_ex_en_milisegundos << endl;
+    cout << "TIEMPO: " << tiempo_ex_en_segundos << endl;
     
     cout << "Cantidad de iteraciones: " << cant_iters << endl;
     if(medir) {
-        archivo_mediciones << tiempo_ex_en_milisegundos << endl;
+        archivo_mediciones << tiempo_ex_en_segundos << endl;
         archivo_mediciones.close();
     }
-   // return autovector;
 }
 
 bool corresponde_usar_extrapolacion(const int cant_iters, const int k) {
      //una vez que tenemos tres iteraciones anteriores, extrapolamos cada k iteraciones
-    //cout << "Voy a extrapolar" << endl;
     return (cant_iters > 3) && (cant_iters % k == 0);
 }
